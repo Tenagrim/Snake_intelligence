686e3fcd35f5528acb449836337187e2a9424212
Tenagrim <tenagrim@gmail.com>
2020-09-02 23:46:47 +0500

    initial commit

---
+++ Snake_Intelligence/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Intelligence
+{
+    class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public static Point operator +(Point a, Point b)
+        {
+            return new Point(a.x + b.x, a.y + b.y);
+        }
+    }
+}
---
+++ Snake_Intelligence/Snake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Intelligence
+{
+    class Snake
+    {
+        public List<Point> Body { get { return body; } }
+
+        private List<Point> body;  
+        public int Point { get; } // 1 - up / -1 - left / 2 - down / -2 - right
+        private Point direction;
+        private Point tmp;
+        public Snake()
+        { 
+            direction = new Point(1,0);
+            tmp = new Point();
+            body = new List<Point>();
+            body.Add(new Point(0, 0));
+            body.Add(new Point(0, 1));
+            body.Add(new Point(0, 2));
+        }
+
+        public void Step()
+        {
+            body[0] = body[0] + direction;
+            int len = body.Count;
+            for (int i = 1; i < len; i++)
+            {
+                body[i] = body[i - 1];
+            }
+
+        }
+    }
+}
